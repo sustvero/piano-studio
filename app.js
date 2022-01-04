6bead093e1dcd8fcc23f6f48c239b9912dade490
sustvero <veronika@sustr.ca>
2022-01-04 18:24:56 -0500

    Add EJS templating and refactor styles.css

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// use routing parameters?
+app.get('/', function(req, res) {
+  res.render("index", {});
+});
+
+app.get('/bio', function(req, res) {
+  res.render("bio", {});
+});
+
+app.get('/contact', function(req, res) {
+  res.render("contact_me", {});
+});
+
+app.get('/lessons', function(req, res) {
+  res.render("lessons", {});
+});
+
+app.get('/relax', function(req, res) {
+  res.render("relax_listen", {});
+});
+
+app.get('/gallery', function(req, res) {
+  res.render("photo_gallery", {});
+});
+
+app.get('/resume', function(req, res) {
+  res.render("resume", {});
+});
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000.");
+});
