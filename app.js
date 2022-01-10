1c59d75d35486da86c05a33608097bc405ed8074
sustvero <veronika@sustr.ca>
2022-01-09 19:09:08 -0500

    Enable server deployment using Heroku

--- app.js
+++ app.js
@@ -37,6 +37,6 @@ app.get('/resume', function(req, res) {
   res.render("resume", {});
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server is running on port 3000.");
 });
